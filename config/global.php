55f6348afb8ef61ec4e022a98ccbae7eb3a6a6da
Ubikfr <ubik@numericable.fr>
2014-04-19 13:34:20 +0200

    Changement du dossier public

--- config/global.php
+++ config/global.php
@@ -4,7 +4,7 @@ define('DS', DIRECTORY_SEPARATOR);
 define('ROOT_DIR', realpath(dirname(__DIR__)));
 define('CONF_DIR', ROOT_DIR.DS.'config'.DS);
 define('UBIK_DIR', ROOT_DIR.DS.'Ubik');
-define('PUB_DIR', ROOT_DIR.DS.'www');
+define('PUB_DIR', ROOT_DIR.DS.'public');
 define('CONTENT', PUB_DIR.DS.'content');
 define('TEMPL_DIR', ROOT_DIR.DS.'templates');
 define('CACHE_DIR', ROOT_DIR.DS.'cache');
