3d63f9e639307569ec1ab1bf12399d1afff2cd57
Ubikfr <ubik@numericable.fr>
2014-04-24 12:49:53 +0200

    Utils RSA

---
+++ Ubik/Utils/RsaCrypt.php
@@ -0,0 +1,43 @@
+<?php
+
+class Utils_RsaCrypt
+{
+    private var $privatekey;
+    public var $publickey = array();
+
+    public function __construct()
+    {
+        
+    }
+
+    public function to_hex($data)
+    {
+        return strtoupper(bin2hex($data));
+    }
+
+    public function decrypt($ciphertext)
+    {
+        if (openssl_private_decrypt(pack('H*', $ciphertext), $plaintext, $this->privatekey)) {
+            return $plaintext;
+        }
+        else {
+            return null;
+        }
+    }
+
+    private function loadKey($num)
+    {
+        $file = file_get_contents(KEYS_DIR.'key_'.$num);
+        $this->privatekey = openssl_pkey_get_private($file);
+    }
+
+    public function getDetails()
+    {
+        $details = openssl_pkey_get_details($this->privatekey);
+        //module n
+        $mod = $this->to_hex($details['rsa']['n']);
+        //exposant public e
+        $pexp = $this->to_hex($details['rsa']['e']);
+        $this->publickey = array('mod'=>$mod,'exp'=>$pexp);
+    }
+}
